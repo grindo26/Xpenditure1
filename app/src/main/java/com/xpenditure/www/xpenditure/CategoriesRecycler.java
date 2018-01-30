19460a41727bd1e5c2586d16bc99f205a84d4e6d
SwarajIngle <swarajingle@gmail.com>
2018-01-30 18:01:39 +0530

    retrieve sucess

---
+++ app/src/main/java/com/xpenditure/www/xpenditure/CategoriesRecycler.java
@@ -0,0 +1,43 @@
+package com.xpenditure.www.xpenditure;
+
+import android.view.View;
+
+/**
+ * Created by Swaraj on 30-01-2018.
+ */
+
+public class CategoriesRecycler {
+    private String Title;
+
+    public CategoriesRecycler() {
+    }
+
+    public CategoriesRecycler(View catgview) {
+    }
+
+    public CategoriesRecycler(String title, String image) {
+
+        Title = title;
+        Image = image;
+    }
+
+    public String getImage() {
+
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    private String Image;
+
+}
